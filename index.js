5ac920556e58be52509302ebf3d91bfe9ebe7027
namicersei <nami09funkstar90@gmail.com>
2018-03-15 11:36:13 +0530

    1st commit

---
+++ index.js
@@ -0,0 +1,10 @@
+<script>
+function add_text(){var input=document.getElementByClassName("col");
+var span=document.createElement("span");
+var node=document.createTextNode(input);
+
+var element=getElementByClassName("num_input");
+element.appendChild(node);}
+
+
+</script>
